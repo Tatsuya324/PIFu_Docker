5b3526bd885dd18720b1ce41c4bfb66c6731944a
Tatsuya Okazaki <180442029@ccmailg.meijo-u.ac.jp>
2021-07-13 21:17:27 +0900

    テスト用スクリプトを追加

--- PIFu_RUN.sh
+++ PIFu_RUN.sh
@@ -70,3 +70,27 @@ case $ANS in
     echo "No"
     ;;
 esac
+
+
+echo "Testを行いますか？[Y/n]:"
+read ANS
+
+case $ANS in
+  "" | [Yy]* )
+    # ここに「Yes」の時の処理を書く
+    if [ -e ${PIFuRUN_DIR}/shared_dir/train_data/GEO ]; then
+        echo "Training用のデータを確認しました。"
+        echo "Trainingを開始します。"
+        . ${PIFuRUN_DIR}/scripts/Test.sh ${PIFuRUN_DIR}/shared_dir
+        echo "Trainingが終了しました。"
+    else
+        echo "Training用のデータが見つかりませんでした。"
+        echo ".${PIFuRUN_DIR}/scripts/DataGeneration.sh を実行してTrainingDataを作成してください。"
+        echo "処理を終了します。"
+    fi
+    ;;
+  * )
+    # ここに「No」の時の処理を書く
+    echo "No"
+    ;;
+esac
